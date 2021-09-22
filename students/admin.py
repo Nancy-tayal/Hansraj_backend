0f73430750025b0a5477d318f22da40aee485b02
Khushboo7701 <68158589+Khushboo7701@users.noreply.github.com>
2021-09-22 15:03:34 +0530

    Student model

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import StudentDetails
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('uid','email','name','university_roll_no','course','semester',)
+    list_filter = ('uid',)
+    search_fields = ('uid',)
+    ordering = ('uid',)
+    filter_horizontal = ()
+
+# Now register the new StudentAdmin...
+admin.site.register(StudentDetails, StudentAdmin)
--- students/models.py
+++ students/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from faculty.models import SubjectDetails
 
 # Create your models here.
 
@@ -7,7 +8,17 @@ User=get_user_model()
 
 class StudentDetails(models.Model):
     uid= models.ForeignKey(User,on_delete=models.CASCADE)
+    email=models.EmailField(max_length=100)
     name=models.CharField(max_length=50,null=False)
     university_roll_no=models.IntegerField(null=False)
     course=models.CharField(max_length=50,null=True)
-    
+    SEM= (
+        (1,1),
+        (2,2),
+        (3,3),
+        (4,4),
+        (5,5),
+        (6,6),
+    )
+    semester=models.IntegerField(choices=SEM,null=True)    
+    subject=models.ManyToManyField(SubjectDetails)
