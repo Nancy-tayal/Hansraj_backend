9f8505cec5aae3615be506200682e1416c1f373a
unknown <aakriti20sbi@gmail.com>
2021-09-22 14:47:25 +0530

    faculty and subject model

--- faculty/models.py
+++ faculty/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User=get_user_model()
+
+class FacultyDetails(models.Model):
+    tid= models.ForeignKey(User,on_delete=models.CASCADE)
+    name=models.CharField(max_length=50,null=False)
+    department=models.CharField(max_length=50,null=False)
+    email=models.CharField(max_length=100,null=False)
+
+class SubjectDetails(models.Model):
+    tid=models.ForeignKey(FacultyDetails, on_delete=models.CASCADE)
+    subject_id=models.AutoField(primary_key = True)
+    subject_name=models.CharField(max_length=50,null=False)
+    
